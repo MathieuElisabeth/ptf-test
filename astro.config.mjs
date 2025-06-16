194af94525452c68e2a6d05346a3e257715d9d4e
MathieuElisabeth <75541968+MathieuElisabeth@users.noreply.github.com>
2025-06-16 10:00:29 +0200

    Fix SSR and import errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ export default defineConfig({
   integrations: [react()],
   vite: {
     ssr: {
-      noExternal: ['styled-components', 'gsap', '@gsap/react']
+      noExternal: ['styled-components', 'gsap', '@gsap/react', '@whatisjery/react-fluid-distortion']
     }
   }
 });
